fc6b9bcdb39e2d45f030792addc31e605507c7bc
w1esther <w1mariaesther@gmail.com>
2025-08-19 10:50:31 -0300

    ajustes parâmetros do triângulo e quadrados adjacentes

--- triangulo_retangulo.py
+++ triangulo_retangulo.py
@@ -4,6 +4,7 @@ class TrianguloRetangulo(Scene):
 # tudo que vai aparecer na tela deve estar dentro de uma Scene
     def construct(self):
     # função responsável por construir a cena
+
         pitagoras = Text('Teorema de Pitágoras', color=BLUE)
         pitagoras.move_to(ORIGIN + UP*3)
 
@@ -19,14 +20,14 @@ class TrianguloRetangulo(Scene):
         self.play(FadeIn(pitagoras))
         # executa animações na cena
         self.wait(2)
-        self.play(triangulo_retangulo.animate.scale(0.75))
+        self.play(triangulo_retangulo.animate.scale(0.5))
         self.wait(2)
 
         numero_ab = Text('3', color=BLUE_B)
-        numero_ab.move_to((a + b)/2 + RIGHT*0.2)
+        numero_ab.move_to((a + b)/2 + RIGHT*0.1 + UP*0.4)
 
         numero_ac = Text('4', color=BLUE_B)
-        numero_ac.move_to((a + c)/2 + LEFT*0.1)
+        numero_ac.move_to((a + c)/2 + RIGHT*0.5)
 
         numero_bc = Text('5', color=BLUE_B)
         numero_bc.move_to((b + c)/2 + RIGHT*0.8)
@@ -36,17 +37,17 @@ class TrianguloRetangulo(Scene):
         self.play(FadeIn(numero_bc))
         self.wait(2)
 
-        quadrado_ab = Square(side_length=3, color=BLUE_D)
-        quadrado_ab.move_to((a + b)/2 + UP*1.5)
+        quadrado_ab = Square(side_length=2.0, color=BLUE_D)
+        quadrado_ab.move_to((a + b)/2 + UP*0.0) 
         self.play (Create(quadrado_ab))
 
-        quadrado_ac = Square(side_length=4, color=BLUE_D)
-        quadrado_ac.move_to((a + c)/2 + LEFT*1.5)
+        quadrado_ac = Square(side_length=2.0, color=BLUE_D)
+        quadrado_ac.move_to((a + c)/2 + RIGHT*0.0)
         self.play(Create(quadrado_ac))
 
-        quadrado_bc = Square(side_length=5, color=BLUE_D)
+        quadrado_bc = Square(side_length=2.5, color=BLUE_D)
         quadrado_bc.rotate(-45 * DEGREES)
-        quadrado_bc.move_to((b + c)/2 + UP*0.7)
+        quadrado_bc.move_to((b + c)/2 + UP*1.3)
         self.play(Create(quadrado_bc))
 
         self.wait(2)
