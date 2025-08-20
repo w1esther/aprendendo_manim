37264b78f49ad8f1cb0634e085b0e31e0501183e
w1esther <w1mariaesther@gmail.com>
2025-08-20 12:18:56 -0300

    atualização da animação - em andamento

--- triangulo_retangulo.py
+++ triangulo_retangulo.py
@@ -6,11 +6,11 @@ class TrianguloRetangulo(Scene):
     # função responsável por construir a cena
 
         pitagoras = Text('Teorema de Pitágoras', color=BLUE)
-        pitagoras.move_to(ORIGIN + UP*3)
+        pitagoras.move_to(ORIGIN + UP*3.5)
 
         a = LEFT*2 + DOWN*2
         b = RIGHT*2 + DOWN*2
-        c = LEFT*2 + UP*2
+        c = LEFT*2 + UP*3
         # definição dos pontos (vértices) do triângulo
 
         triangulo_retangulo = Polygon(a, b, c, color=BLUE)
@@ -38,16 +38,31 @@ class TrianguloRetangulo(Scene):
         self.wait(2)
 
         quadrado_ab = Square(side_length=2.0, color=BLUE_D)
-        quadrado_ab.move_to((a + b)/2 + UP*0.0) 
+        quadrado_ab.move_to((a + b)/2 + UP*1.0) 
         self.play (Create(quadrado_ab))
 
-        quadrado_ac = Square(side_length=2.0, color=BLUE_D)
+        quadrado_ac = Square(side_length=3.0, color=BLUE_D)
         quadrado_ac.move_to((a + c)/2 + RIGHT*0.0)
         self.play(Create(quadrado_ac))
 
-        quadrado_bc = Square(side_length=2.5, color=BLUE_D)
-        quadrado_bc.rotate(-45 * DEGREES)
-        quadrado_bc.move_to((b + c)/2 + UP*1.3)
+        quadrado_bc = Square(side_length=2.8, color=BLUE_D)
+        quadrado_bc.rotate(-60 * DEGREES)
+        quadrado_bc.move_to((b + c)/2 + UP*1.0 + RIGHT*1.0)
         self.play(Create(quadrado_bc))
+
+        self.wait(2)
+        self.play(FadeOut(numero_ab))
+        self.play(FadeOut(numero_ac))
+        self.play(FadeOut(numero_bc))
+        self.wait(2)
+        
+        linha1 = Line(start=LEFT*3, end=RIGHT*-1.0, color=BLUE)
+        self.play(Create(linha1))
+        linha2 = Line(start=LEFT*12, end=RIGHT*-6.0, color=BLUE)
+        linha2.move_to(ORIGIN + UP*0.5)
+        self.play(Create(linha2))
+        linha3 = Line(start=LEFT*12, end=RIGHT*-6.0, color=BLUE)
+        linha3.move_to(ORIGIN + DOWN * 0.5)
+        self.play(Create(linha3))
 
         self.wait(2)
