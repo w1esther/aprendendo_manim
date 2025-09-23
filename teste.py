5b838c6fb3391ffc51459c81bdd7ca988dc01d2b
w1esther <w1mariaesther@gmail.com>
2025-09-22 21:11:18 -0300

    atualizações e comentarios

--- teste.py
+++ teste.py
@@ -19,6 +19,8 @@ class PitagorasBloquinhos(Scene):
         sq_a = Square(side_length=a * 0.5, color=BLUE).next_to(tri, DOWN, buff=0, aligned_edge=LEFT)
         sq_b = Square(side_length=b * 0.5, color=GREEN).next_to(tri, LEFT, buff=0, aligned_edge=DOWN)
         sq_c = Square(side_length=c * 0.5, color=RED).move_to([a/2, b/2, 0] + LEFT*0.7 + DOWN*0.22 + RIGHT*0.25).rotate((120.0))
+        #atenção no uso do rotate
+        #obs usae next_to para tentar fazer alinhamento
 
         self.play(Create(sq_a), Create(sq_b), Create(sq_c))
         self.wait(1)
@@ -31,6 +33,8 @@ class PitagorasBloquinhos(Scene):
                 bloco.move_to(sq_a.get_corner(DL) + RIGHT*(i+0.5)*0.5 + UP*(j+0.5)*0.5)
                 bloquinhos.add(bloco)
 
+        #sq_a.get_corner(DL) retorna a coordenada do canto inferior-esquerdo (DL = down-left) do quadrado sq_a.
+
         for i in range(b):
             for j in range(b):
                 bloco = Square(side_length=0.5, fill_color=GREEN, fill_opacity=0.7, stroke_width=0)
@@ -41,19 +45,20 @@ class PitagorasBloquinhos(Scene):
 
         self.wait(2)
         
-        bloquinhos2 = bloquinhos.rotate(125*DEGREES)
-
+        bloquinhos2 = bloquinhos.copy().rotate(125*DEGREES)
         self.play(Transform(bloquinhos, bloquinhos2))
         # Posicionamento alvo dos bloquinhos no quadrado da hipotenusa
+
         destinos = VGroup()
         n = int(c)
         for i in range(n):
             for j in range(n):
                 bloco = Square(side_length=0.5, stroke_width=0)
-                # alinhar dentro do quadrado da hipotenusa
+                # Criamos um quadradinho invisível (sem borda e sem preenchimento). Ele não vai aparecer na cena, mas serve como um alvo.
                 pos = sq_c.get_center() + (i - n/2 + 0.5) * RIGHT + (j - n/2 + 0.5) * UP
                 bloco.move_to(pos * 0.5)
                 destinos.add(bloco)
+                
         destinos.rotate(125* DEGREES, about_point=sq_c.get_center())
 
         # Animação: bloquinhos se movem até formar c^2
